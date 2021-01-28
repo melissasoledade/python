2b5bdefdfff16b5383d994f53d00e4b5a419de0d
melissasoledade <melissa.gsoledade@gmail.com>
2021-01-27 21:28:51 -0300

    search-linkedin-links

--- read_linkedin.py
+++ read_linkedin.py
@@ -1,30 +1,43 @@
 # Ler arquivo para encontrar links do linkedin
+# Gravar links em txt
+
 import codecs
+import re
 
 def read_file():
-	#with open("capacita_mdt.txt", encoding ="utf8") as f:
-	#	read_file = f.read()
+	text = ['a']
+
 	types_of_encoding = ["utf8", "cp1252", "cp850", "utf-8"]
 	for i in types_of_encoding:
 		with codecs.open("capacita_mdt.txt", encoding = i, errors ='replace') as f:
 			try:
-				read_file = f.read()
+				read_file = f.readlines()
 				break
 			except UnicodeDecodeError:
 				pass
 	f.close()
 
-	return read_file
+	for line in read_file:
+		text.append(line.split())
+
+	return text
 
 def find_string(file, string):
+	links = []
 	count = 0
-	for i in file:
-		
-			#https://www.linkedin.com/in/amanda-fernandes-2125b83b/
-			#[https://https://www.linkedin.com]
+	for line in file:
+		for i in line:
+			if(string in i):				
+				links.append(i)
+				count += 1
+	with open("links_linkedin.txt", "w") as f:
+		for link in links:
+			f.write("%s\n" %link)
+
+	return count
 
 if __name__ == "__main__":
     file = read_file()
-    #find_string(file, "www.linkedin.com")
+    links = find_string(file, "www.linkedin.com")
+    print("Quantidade de links gravados: %d\n" % links)
     #print(file)
-    print(type(file))
